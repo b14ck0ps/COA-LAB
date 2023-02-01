20e46045c8a2c709beb51c0734cb55046863a82c
Azran Hossain <mr69751@gmail.com>
2023-02-01 09:07:01 +0600

    output with newline

--- LAB 2/Basic-input-outpjut.asm
+++ LAB 2/Basic-input-outpjut.asm
@@ -12,11 +12,23 @@ org 100h
 .CODE ; CODE SECTION
 MAIN PROC  
     
-    MOV AL, 'A'
+    MOV AL, 'A'                
     
+    
+    ;this is the input section
     MOV AH, 1 ; AH, 1 FOR TAKIKG INPUT
+    INT 21H   ; always call this interupt
+    MOV BL,AL ; Save input to BL reg
+    
+    
+    ;this is for new print line
+    MOV AH,2
+    MOV DL, 0AH
     INT 21H
-    MOV BL,AL 
+    MOV DL,0DH ; carries return for new line otw some there are some space before new line
+    INT 21H
+    
+    ; this is the print section
     MOV AH, 2 ; AH, 2 IS FOR OUTPUT
     MOV DL,BL; DX DISPLAY THE VARS
     INT 21H ; ALWAYS CALL THIS AFTER DISPLAY.. INTERUPT 21H
