cd2f38c001398a29ed89ef02f69c3e973519dbf8
Azran Hossain <mr69751@gmail.com>
2023-02-08 09:00:39 +0600

    print your name

---
+++ LAB 3/Tasks/Task1_print_your_name.asm
@@ -0,0 +1,22 @@
+
+.MODEL SMALL
+.STACK 100H
+.DATA
+
+; ASCII value set by comma ',' and full string will be inside a single ' and end with a $ sign
+MSG DB  'AJRAN$' 
+
+.CODE
+
+MAIN PROC 
+    MOV AX, @DATA
+    MOV DS, AX
+    
+    LEA DX, MSG ; LEA return address of this string
+    MOV AH, 9  ; This line will print the full string
+    INT 21H
+    
+    MOV AH,4CH 
+    INT 21H
+    MAIN ENDP
+END MAIN
---
+++ LAB 3/Tasks/Task1_print_your_name_with_single_char copy.asm
@@ -0,0 +1,56 @@
+
+org 100h
+
+.MODEL SMALL
+.STACK 100H
+
+.DATA   ;STORE VARIABLES 
+
+A DW 'A'
+B DW 'J'
+C DW 'R'
+D DW 'A'
+E DW 'N'
+
+
+
+
+.CODE ; CODE SECTION
+
+MAIN PROC     
+    
+    MOV AX, [A] 
+
+    MOV AH, 2 
+    MOV DX,AX
+    INT 21H 
+
+    MOV AX, [B]
+    MOV AH, 2
+    MOV DX,AX
+    INT 21H
+
+    MOV AX, [C]
+    MOV AH, 2
+    MOV DX,AX
+    INT 21H
+
+    MOV AX, [D]
+    MOV AH, 2
+    MOV DX,AX
+    INT 21H
+
+    MOV AX, [E]
+    MOV AH, 2
+    MOV DX,AX
+    INT 21H
+    
+    MOV AH,4CH
+    INT 21H
+    MAIN ENDP
+       
+END MAIN
+
+ret
+
+
