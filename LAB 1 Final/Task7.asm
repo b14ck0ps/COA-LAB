c2385c978cae4b048db92d0b06cafd108a6b7808
Azran Hossain <mr69751@gmail.com>
2023-03-15 10:09:52 +0600

    making some sense using comments

--- LAB 1 Final/Task7.asm
+++ LAB 1 Final/Task7.asm
@@ -2,7 +2,10 @@
 .stack 100h
 .data
     msg db 'Enter a string: $'
-    str db 20,?,20 dup('$')
+    ;20 at the begining is byte size of this var 20byte
+    ;? is unknown cz we taking it from user
+    ;20 after ? is how much time dup() will repeat
+    str db 20,?,20 dup('$') ; dup() is a operand for repeat a cirtain db / dw. normally use for making array
 .code
 main proc
 
@@ -19,7 +22,7 @@ main proc
     int 21h
 
     mov ah, 09h
-    lea dx, str+2
+    lea dx, str+2 ; cz we know array is dw is address + 2
     int 21h
 
     mov ah, 4ch
